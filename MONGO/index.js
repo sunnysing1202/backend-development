027a3cbce36e7cb7bebd2bc1cda7aea2d1ab6769
Sunny Kumar <sunnykr150401@gmail.com>
2024-11-26 19:29:08 +0530

    added new folder MONGO & and done basic connection

---
+++ MONGO/index.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+main()
+    .then((res) => {
+        // console.log(res);
+        console.log("connection successful");
+    })
+    .catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+}
